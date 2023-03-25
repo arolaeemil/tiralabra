c17c440209cf0a4c3285d642c8bb16f050acb4e7
Eemil Arola <eemil.arola@gmail.com>
2023-03-25 18:33:16 +0200

    tehty pieniä muutoksia ja yritetty varmistua tulosten oikeellisuudesta.

--- src/index.py
+++ src/index.py
@@ -2,6 +2,8 @@ from tekstinkasittelijaluokkana import Tekstinkasittelija
 from triepuu import Triepuu
 import os
 
+#testausta on aika paljon tässä kommentoituna, anteeksi siitä, on väliaikaista
+
 def main():
     #tiedostonimi = "helppotesti.txt"
     #tiedostonimi = "/home/eemil/Desktop/tiralabra/tiralabra/src/helppotesti.txt"
@@ -22,44 +24,49 @@ def main():
     testisanat3 = tekstinkasittelija.sanakirja3
     testipuu2 = Triepuu()
     testipuu3 = Triepuu()
-    testipuu2.lisaa_sanat(testisanat2)
-    testipuu3.lisaa_sanat(testisanat3)
+    #testipuu2.lisaa_sanat(testisanat2)
+    #testipuu3.lisaa_sanat(testisanat3)
+    #siistimpi metodi
+    print(testisanat2)
+    print(testisanat3)
+    testipuu2.lisaa_sanat_test(testisanat2)
+    testipuu3.lisaa_sanat_test(testisanat3)
     #print(testipuu.juuri.get_lapsi("tämä").get_lapsi("on"))
 
     #testipuu2.tarkista_puu2()
     #testipuu3.tarkista_puu3()
 
-    print(testipuu3.juuri.get_lapsi("tämä").get_lapsi("on").get_lapsi("testi"))
-    print(testipuu3.juuri.get_lapsi("tämä").get_lapsi("on").get_lapsi("testi2").lehti)
+    #rint(testipuu3.juuri.get_lapsi("tämä").get_lapsi("on").get_lapsi("testi"))
+    #print(testipuu3.juuri.get_lapsi("tämä").get_lapsi("on").get_lapsi("testi2").lehti)
 
-    print(testipuu3.juuri.get_lapsi("toistan").get_lapsi("tämä").get_lapsi("on"))
-    print(testipuu3.juuri.get_lapsi("toistan").get_lapsi("tämä").get_lapsi("on").lehti)
+    #print(testipuu3.juuri.get_lapsi("toistan").get_lapsi("tämä").get_lapsi("on"))
+    #print(testipuu3.juuri.get_lapsi("toistan").get_lapsi("tämä").get_lapsi("on").lehti)
 
-    print(testipuu3.juuri.get_lapsi("testi2").get_lapsi("tämä"))
-    print(testipuu3.juuri.get_lapsi("testi2").get_lapsi("tämä").lehti)
+    #print(testipuu3.juuri.get_lapsi("testi2").get_lapsi("tämä"))
+    #print(testipuu3.juuri.get_lapsi("testi2").get_lapsi("tämä").lehti)
 
-    print("################")
+    #print("################")
 
-    x = testipuu3.hae_puusta("tämä")
-    print(x.lapset.keys())
-    x = testipuu3.hae_puusta("on",x)
-    print(x.lapset.keys())
-    print(testipuu3.hae_puusta("testi",x))
+    #x = testipuu3.hae_puusta("tämä")
+    #print(x.lapset.keys())
+    #x = testipuu3.hae_puusta("on",x)
+    #print(x.lapset.keys())
+    #print(testipuu3.hae_puusta("testi",x))
 
-    print("################")
+    #print("################")
     
-    y = testipuu3.hae_puusta("on")
-    print(y.lapset.keys())
-    y = testipuu3.hae_puusta("testi",y)
-    print(y.lapset.keys())
-    print(testipuu3.hae_puusta("toistan",y))
+    #y = testipuu3.hae_puusta("on")
+    #print(y.lapset.keys())
+    #y = testipuu3.hae_puusta("testi",y)
+    #print(y.lapset.keys())
+    #print(testipuu3.hae_puusta("toistan",y))
 
-    print("################")
+    #print("################")
 
     print(testipuu2.get_koko())
     print(testipuu3.get_koko())
 
-    print(tekstinkasittelija.sanakirja2.keys())
+    #print(tekstinkasittelija.sanakirja2.keys())
 
 if __name__ == "__main__":
     main()
--- src/triepuu.py
+++ src/triepuu.py
@@ -23,6 +23,44 @@ class Triepuu:
                     tama_solmu  = tama_solmu.get_lapsi(sana)
                 tama_solmu.lehti = True
                 self.koko = self.koko + 1
+    
+    #ehkä se parempi metodi?
+    def lisaa_sanat_test(self,sanakirja):
+        avaimet = sanakirja.keys()
+        for avain in avaimet:
+            tama_solmu = self.juuri
+            sanajonot = sanakirja[avain]
+            indeksi = 0
+            #yhtä avainta voi vastata lista monkoita. Monikoissa on monikon sanat
+            #listan ensimmäisessä alkiossa ja monikon esiintymisenkerrat toisessa.
+            for monikko in sanajonot:
+                #print(monikko)
+                sanajono = self.tee_sanoiksi(sanajonot,indeksi)
+                indeksi = indeksi + 1
+                montako_sanaa = len(sanajono)
+                tama_solmu.lisaa_lapsi(avain,Triesolmu(avain))
+                tama_solmu = tama_solmu.get_lapsi(avain)
+                #i = 1, sillä sanajonon ensimmäinen sana on avain itse
+                i = 1
+                while i < montako_sanaa:
+                    tama_solmu.lisaa_lapsi(sanajono[i],Triesolmu(sanajono[i]))
+                    tama_solmu = tama_solmu.get_lapsi(sanajono[i])
+                    i = i + 1
+                #ollaanko ensimmäistä kertaa "polun" päässä
+                if tama_solmu.lehti == False:
+                    self.koko = self.koko + 1
+                tama_solmu.lehti = True
+
+    def tee_sanoiksi(self, sanajono, indeksi):
+        lista = []
+        stringi = sanajono[indeksi][0]
+        #print(stringi)
+        stringi = stringi.split()
+        for alkio in stringi:
+            lista.append(alkio)
+        return lista
+
+
 
     def etsi_solmu(self, stringi):
         if len(stringi) == 0:
